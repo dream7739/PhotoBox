0a7a785ee4d1dea080e9bc48d25dc7091284a8a7
홍정민 <dream7739@naver.com>
2024-07-27 21:09:16 +0900

    enum 정리 dateFormatter 별도 enum으로 관리 좋아요 시 ToastMessage 출력되는 기능 추가 게시일, 사진 사이즈 연산 프로퍼티로 관리되도록 변경

---
+++ Polaroid/Database/RealmProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  RealmProtocol.swift
+//  Polaroid
+//
+//  Created by 홍정민 on 7/27/24.
+//
+
+import Foundation
---
+++ Polaroid/Manager/DateFormatterManager.swift
@@ -0,0 +1,8 @@
+//
+//  DateFormatterManager.swift
+//  Polaroid
+//
+//  Created by 홍정민 on 7/27/24.
+//
+
+import Foundation
